3cf5459b9537c4d80567591c543457a7336aec45
JaeSeo Yang <qud1251@gmail.com>
2023-06-27 23:23:50 +0900

    boj 17276 - 코드 마음에 안 들어서 로직 수정 중

---
+++ Implementation/17276 - 배열 돌리기/main.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+
+#define endl '\n'
+#define MAX 501
+
+using namespace std;
+
+int arr[MAX][MAX];
+int ans[MAX][MAX];
+
+int main() {
+    ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    
+    int t; cin >> t;
+    int n, d; 
+    int i, j;
+    int cnt;
+
+    while (t--) {
+        cin >> n >> d;
+        for (i=0; i<n; i++) for (j=0; j<n; j++) cin >> arr[i][j]; 
+
+        cnt = (d >= 0) ? d / 45 : (d / 45) + 4;
+        while(cnt--) {
+            for (i=0; i<n; i++) ans[i][(n+1)/2 - 1] = arr[i][i];
+            for (i=0; i<n; i++) ans[i][i] = arr[(n+1)/2 - 1][i];
+            for (i=n-1; i>=0; i--) ans[(n+1)/2 - 1][n-1-i] = arr[i][n-1-i];
+            for (i=n-1; i>=0; i--) ans[i][n-1-i] = arr[i][(n+1)/2 - 1];
+            memcpy(arr, ans, sizeof(ans));
+        }
+
+        for (i = 0; i < n; i++) {
+            for (j = 0; j < n; j++) cout << ans[i][j] << " ";
+            cout << endl;
+        } 
+    }
+
+    return 0;
+}
