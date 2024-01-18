750fde8c16a99f1a4227bbdeaa6c209322c2bd3c
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-01-19 03:19:35 +0900

    boj-2469

--- algorithm/2469 - 사다리 타기/main.cpp
+++ algorithm/2469 - 사다리 타기/main.cpp
@@ -5,14 +5,63 @@
 using namespace std;
 
 int k, n;
-string pre = "ABCDEFGHIJKLMNOPQRXTUVWXYZ", target;
-vector<string> ladder;
+
+string matchLadder(string pre, string target) {
+    string res;
+
+    for (int i=0; i<k-1; ++i) {
+        if (pre[i] == target[i]) res += '*';
+        else if (pre[i] == target[i+1]) {
+            res += (i != k-2) ? "-*" : "-";
+            ++i;
+        }
+        else {
+            string cannot;
+            for (int i=0; i<k-1; ++i) cannot += 'x';
+            return cannot;
+        }
+    }
+    return res;
+}
+
+void undergo(string& people, string& ladder) {
+    for (int i=0; i<k-1; ++i) {
+        if (ladder[i] == '*') continue;
+        char tmp = people[i];
+        people[i] = people[i+1];
+        people[i+1] = tmp;
+    }
+}
 
 int main() {
     ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    string pre, target;
     cin >> k >> n >> target;
-    ladder.resize(n);
-    for (int i = 0; i < n; ++i) cin >> ladder[i];
+
+    for (int i=0; i<k; ++i) pre += (65 + i);
+
+    int unknown;
+    bool flag = false;
+    vector<string> ladders;
+    for (int i = 0; i < n; ++i) {
+        string now; cin >> now;
+
+        if (flag) ladders.push_back(now);
+
+        if (now[0] == '?') {
+            unknown = i;
+            flag = true;
+            continue;
+        }
+
+        if (!flag) undergo(pre, now);
+    }
+
+    for (int idx = ladders.size() - 1; idx >= 0; --idx) {
+        undergo(target, ladders[idx]);
+    }
+
+    cout << matchLadder(pre, target);
 
     return 0;
 }
