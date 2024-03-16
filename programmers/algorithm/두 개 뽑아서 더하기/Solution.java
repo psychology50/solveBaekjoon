f308614172c1a1d2239174bd6da916c15a67547e
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2024-03-16 15:49:52 +0900

    programmer-두 개 뽑아서 더하기

---
+++ programmers/algorithm/두 개 뽑아서 더하기/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int[] numbers) {
+        int n = numbers.length;
+        Set<Integer> set = new HashSet<>();
+        for (int i=0; i<n; ++i) for (int j=i+1; j<n; ++j) {
+            set.add(numbers[i] + numbers[j]);
+        }
+        
+        int[] answer = new int[set.size()];
+        int i = -1;
+        for (Integer s : set) answer[++i] = s;
+        Arrays.sort(answer);
+        return answer;
+    }
+}
