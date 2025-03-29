11668df17ac9597601c850d2c9f0ae8bb3cf0790
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-03-29 23:01:52 +0900

    programmers-부족한 금액 계산하기

---
+++ programmers/algorithm/부족한 금액 계산하기/Main.java
@@ -0,0 +1,16 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        long total=0;
+		
+        for(int i=1; i<count+1; i++){
+            total += price*i;
+        }
+        if(money<total)
+            answer = total-money;
+        else
+            answer = 0;
+        
+        return answer;
+    }
+}
