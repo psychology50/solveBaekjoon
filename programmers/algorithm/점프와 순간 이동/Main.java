d9db77cc1d970ff40b085a829a5ac71d7b6216ae
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-05-07 20:00:40 +0900

    programmers-점프와 순간 이동

---
+++ programmers/algorithm/점프와 순간 이동/Main.java
@@ -0,0 +1,15 @@
+public class Solution {
+    public int solution(int n) {
+        int ans = 0;
+
+        while (n != 0) {
+            if (n % 2 == 1) {
+                ans++;
+            }
+
+            n /= 2;
+        }
+
+        return ans;
+    }
+}
