16128baa0f886445c57bef1baced7e5610ff7cfc
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-05-09 20:17:22 +0900

    programmers-멀리 뛰기

---
+++ programmers/algorithm/멀리 뛰기/Main.java
@@ -0,0 +1,14 @@
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        long[] dp = new long[n + 2];
+        dp[0] = 0;
+        dp[1] = 1;
+        dp[2] = 2;
+        
+        for (int i = 3; i <= n ; i++) {
+            dp[i] = (dp[i - 1] + dp[i - 2])%1234567;
+        }
+        return dp[n];
+    }
+}
