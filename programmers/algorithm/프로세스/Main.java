89cc879d9b4d5fbbcce9d74777df660ea25a9504
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-05-29 20:19:08 +0900

    programmers-프로세스

---
+++ programmers/algorithm/프로세스/Main.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] priorities, int location) {
+        PriorityQueue<Integer> queue = new PriorityQueue<>(Collections.reverseOrder());
+        int answer = 0;
+
+        for (int i : priorities) {
+            queue.offer(i);
+        }
+
+        while (!queue.isEmpty()) {
+            for (int i = 0; i < priorities.length; i++) {
+                if (queue.peek() == priorities[i]) {
+                    queue.poll();
+                    answer++;
+
+                    if (location == i) {
+                        return answer;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+}
