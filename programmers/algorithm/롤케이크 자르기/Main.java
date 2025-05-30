7e50d8109976bea56aaa2e6e095fa8dff61cdf89
JaeSeo Yang <96044622+psychology50@users.noreply.github.com>
2025-05-30 23:32:45 +0900

    programmers-롤케이크 자르기

---
+++ programmers/algorithm/롤케이크 자르기/Main.java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] topping) {
+        int answer = 0;
+        
+        Map<Integer, Integer> map_1 = new HashMap<>();
+        Map<Integer, Integer> map_2 = new HashMap<>();
+        
+        for (int e : topping) {
+            map_2.put(e, map_2.getOrDefault(e, 0) + 1);
+        }
+        
+        for (int e : topping) {
+            map_1.put(e, map_1.getOrDefault(e, 0) + 1);
+            
+            if (map_2.get(e) - 1 == 0) {
+                map_2.remove(e);                
+            } else {
+                map_2.put(e, map_2.get(e) - 1);  
+            }
+            
+            if (map_1.size() == map_2.size()) {
+                answer++;
+            }
+        }
+
+        return answer;
+    }
+}
